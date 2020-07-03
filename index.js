3842037b8dcbfef2c1c7e76c01b26e13d852dc6c
anxiubin <tnqls0120@naver.com>
2020-07-03 14:08:18 +0900

    add Login feature with Bcrypt and Token feature with jsonwebtoken

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
+const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 
 const { User } = require("./models/User")
@@ -12,6 +12,7 @@ const port = 5000
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, 
@@ -30,6 +31,36 @@ app.post('./register', (req, res) => {
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err})
     return res.status(200).json({ success: true })
+  })
+})
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제출된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) 
+        return res.json({ 
+          loginSuccess: false,
+          message: "제출된 비밀번호가 틀렸습니다."
+        })
+
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+
+        res.cookie("x_auth" user.token)
+          .status(200)
+          .json({ 
+            loginSuccess: true, 
+            userId: user._id 
+          })
+      })
+    })
   })
 })
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const { JsonWebTokenError } = require('jsonwebtoken')
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
   name: {
@@ -19,7 +22,7 @@ const userSchema = mongoose.Schema({
     maxlength: 50
   },
   role: {
-    type: String,
+    type: Number,
     default: 0
   },
   image: String,
@@ -30,6 +33,43 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save', function(next) {
+  var user = this
+
+  if(user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err)
+  
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err)
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return callback(err),
+      callback(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(callback) {
+  var user = this
+
+  var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+  user.token = token
+  user.save(function(err,user) {
+    if(err) return callback(err)
+      callback(null, user)
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
