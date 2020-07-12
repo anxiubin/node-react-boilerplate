6310787524f217f03e5a96e85515134cbd090ab7
anxiubin <tnqls0120@naver.com>
2020-07-12 19:43:14 +0900

    add Login Page using redux

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100vh;
+`
 
 function LandingPage() {
-    return (
-        <div>
-            LandingPage
-        </div>
-    )
+	return (
+		<Wrapper>
+			<h2>Landing Page</h2>
+		</Wrapper>
+	)
 }
 
 export default LandingPage
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { loginUser } from '../../../_actions/user_actions'
+import { useDispatch } from 'react-redux'
 
-function LoginPage() {
-    return (
-        <div>
-            LoginPage
-        </div>
-    )
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100vh;
+`
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+`
+
+function LoginPage(props) {
+
+	const dispatch = useDispatch()
+
+	const [Email, setEmail] = useState("")
+	const [Password, setPassword] = useState("")
+
+	const onEmailHandler = (event) => {
+		setEmail(event.currentTarget.value)
+	}
+	
+	const onPasswordHandler = (event) => {
+		setPassword(event.currentTarget.value)
+	}
+
+	const onSubmitHandler = (event) => {
+		event.preventDefault()
+
+		let body = {
+			email: Email,
+			password: Password
+		}
+
+		dispatch(loginUser(body))
+			.then(res => {
+				if(res.payload.loginSuccess) {
+					props.history.push('/')
+				} else {
+					alert('Error')
+				}
+			})
+	}
+
+	return (
+		<Wrapper>
+				
+			<Form onSubmit={onSubmitHandler}>
+					<label>Email</label>
+					<input type="email" value={Email} onChange={onEmailHandler} />
+					<label>Password</label>
+					<input type="password" value={Password} onChange={onPasswordHandler} />
+					<br />
+					<button>Login</button>
+			</Form>
+				
+		</Wrapper>
+	)
 }
 
 export default LoginPage
