9457b43e4ef17be5628c34b289c717800240a070
anxiubin <tnqls0120@naver.com>
2020-10-20 22:45:52 +0900

    add NavBar and Footer

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react"
 
 function Footer() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				fontSize: "0.5rem",
+				padding: 20,
+			}}
+		>
+			<div>made by anxiubin</div>
+		</div>
+	)
 }
 
 export default Footer
