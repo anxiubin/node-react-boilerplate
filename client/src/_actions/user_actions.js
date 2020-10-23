7669785e54836efe9dd0901d0b6c337d0c2ceb9d
anxiubin <tnqls0120@naver.com>
2020-10-23 23:21:45 +0900

    add LogOut action and reducer

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -1,35 +1,44 @@
-import axios from 'axios'
-import { LOGIN_USER, REGISTER_USER, AUTH_USER } from './types'
+import axios from "axios"
+import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from "./types"
+
+const USER_SERVER = "/api/users"
 
 export function loginUser(dataToSubmit) {
+	const request = axios
+		.post(`${USER_SERVER}/login`, dataToSubmit)
+		.then((res) => res.data)
 
-	const request = axios.post('/api/users/login', dataToSubmit)
-		.then(res => res.data)
-		
-		return {
-			type: LOGIN_USER,
-			payload: request
-		} 
-} 
+	return {
+		type: LOGIN_USER,
+		payload: request,
+	}
+}
 
 export function registerUser(dataToSubmit) {
-	
-	const request = axios.post('/api/users/register', dataToSubmit)
-		.then(res => res.data)
-		
-		return {
-			type: REGISTER_USER,
-			payload: request
-		} 
-} 
+	const request = axios
+		.post(`${USER_SERVER}/register`, dataToSubmit)
+		.then((res) => res.data)
+
+	return {
+		type: REGISTER_USER,
+		payload: request,
+	}
+}
 
 export function auth() {
-	
-	const request = axios.get('/api/users/auth')
-		.then(res => res.data)
-		
-		return {
-			type: AUTH_USER,
-			payload: request
-		} 
-} 
+	const request = axios.get(`${USER_SERVER}/auth`).then((res) => res.data)
+
+	return {
+		type: AUTH_USER,
+		payload: request,
+	}
+}
+
+export function logoutUser() {
+	const request = axios.get(`${USER_SERVER}/logout`).then((res) => res.data)
+
+	return {
+		type: LOGOUT_USER,
+		payload: request,
+	}
+}
--- client/src/_reducers/user_reducers.js
+++ client/src/_reducers/user_reducers.js
@@ -1,19 +1,27 @@
-import { LOGIN_USER, REGISTER_USER, AUTH_USER} from '../_actions/types'
+import {
+	LOGIN_USER,
+	REGISTER_USER,
+	AUTH_USER,
+	LOGOUT_USER,
+} from "../_actions/types"
 
 export default function (state = {}, action) {
-  switch (action.type) {
-    case LOGIN_USER:
-        return { ...state, loginSuccess: action.payload }
-        break
+	switch (action.type) {
+		case LOGIN_USER:
+			return { ...state, loginSuccess: action.payload }
+			break
 
-    case REGISTER_USER:
-        return { ...state, register: action.payload }
-        break
+		case REGISTER_USER:
+			return { ...state, register: action.payload }
+			break
 
-    case AUTH_USER:
-        return { ...state, userData: action.payload }
+		case AUTH_USER:
+			return { ...state, userData: action.payload }
 
-    default:
-        return state
-  }
+		case LOGOUT_USER:
+			return { ...state }
+
+		default:
+			return state
+	}
 }
